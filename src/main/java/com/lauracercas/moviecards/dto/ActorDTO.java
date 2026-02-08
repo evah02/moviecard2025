a63ad54d76482bf9b730fa8ba197d589a8b313bc
Eva Huertas <e.huertas@uah.es>
2026-02-08 20:34:12 +0100

    quitando getters y setters con lombok

--- src/main/java/com/lauracercas/moviecards/dto/ActorDTO.java
+++ src/main/java/com/lauracercas/moviecards/dto/ActorDTO.java
@@ -1,9 +1,12 @@
 package com.lauracercas.moviecards.dto;
 
 import java.util.Date;
-
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
+@Getter
+@Setter
 public class ActorDTO {
     private Integer id;
     private String name;
@@ -12,44 +15,4 @@ public class ActorDTO {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date deathDate;
     private String country;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Date getDeathDate() {
-        return deathDate;
-    }
-
-    public void setDeathDate(Date deathDate) {
-        this.deathDate = deathDate;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
 }
--- src/main/java/com/lauracercas/moviecards/dto/MovieDTO.java
+++ src/main/java/com/lauracercas/moviecards/dto/MovieDTO.java
@@ -1,6 +1,11 @@
 package com.lauracercas.moviecards.dto;
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MovieDTO {
     private Integer id;
     private String title;
@@ -12,75 +17,4 @@ public class MovieDTO {
     private String sinopsis;
 
     private List<ActorDTO> actors;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Integer getReleaseYear() {
-        return releaseYear;
-    }
-
-    public void setReleaseYear(Integer releaseYear) {
-        this.releaseYear = releaseYear;
-    }
-
-    public Integer getDuration() {
-        return duration;
-    }
-
-    public void setDuration(Integer duration) {
-        this.duration = duration;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getSinopsis() {
-        return sinopsis;
-    }
-
-    public void setSinopsis(String sinopsis) {
-        this.sinopsis = sinopsis;
-    }
-
-    public List<ActorDTO> getActors() { 
-        return actors; 
-    }
-    public void setActors(List<ActorDTO> actors) { 
-        this.actors = actors; 
-    }
 }
